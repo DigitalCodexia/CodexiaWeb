b8e3a3d9d7704f84d418cd5b54fba19578a13d69
sanchex-22 <david21.cs70@gmail.com>
2026-03-12 15:13:00 -0500

    analiticas

--- app/page.tsx
+++ app/page.tsx
@@ -4,17 +4,18 @@ import { TrustBar } from "@/components/trust-bar"
 import { Services } from "@/components/services"
 import { Process } from "@/components/process"
 import { Benefits } from "@/components/benefits"
-
 import { FAQ } from "@/components/faq"
 import { CtaFinal } from "@/components/cta-final"
 import { ContactForm } from "@/components/contact-form"
 import { Footer } from "@/components/footer"
 import { WhatsAppFloat } from "@/components/whatsapp-float"
 import { Portfolio } from "@/components/portfolio"
+import { Analytics } from "@vercel/analytics/next"
 
 export default function Home() {
   return (
     <main>
+      <Analytics/>
       <Navbar />
       <Hero />
       <TrustBar />
