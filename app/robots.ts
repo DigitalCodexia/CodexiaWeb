a57ac1ada0e4849c6d4d286c8853e068a2af5136
Carlos Sanchez <david21.cs70@gmail.com>
2026-04-16 20:24:09 -0500

    robots

--- app/robots.ts
+++ app/robots.ts
@@ -1,6 +1,6 @@
 import { MetadataRoute } from 'next'
 
-const BASE_URL = 'https://digitalcodexia.com'
+const BASE_URL = 'https://www.digitalcodexia.com'
 
 export default function robots(): MetadataRoute.Robots {
   return {
